837bfc4eb733566e0cc421ba25204e1b075c7d1d
MateusSilva0405 <mat673766@Gmail.com>
2026-03-18 14:51:26 -0300

    Sem erros e formulario/litar feitos

--- src/main/java/br/com/petweb/petweb/controller/ClienteController.java
+++ src/main/java/br/com/petweb/petweb/controller/ClienteController.java
@@ -3,10 +3,12 @@ package br.com.petweb.petweb.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -17,7 +19,9 @@ import br.com.petweb.petweb.service.ClienteService;
 @RequestMapping("/clientes")
 public class ClienteController {
     
+    
     // Injeção de dependência da service de clientes
+    @Autowired
     private ClienteService clienteService;
 
     public ClienteController(ClienteService clienteService){
@@ -43,6 +47,21 @@ public class ClienteController {
     @GetMapping("/criar")
     public String criar(Model model) {
         model.addAttribute("cliente", new Cliente());
+        return "cliente/formularioCliente";
+    }
+
+    // Método para excluir cliente
+    @GetMapping("/excluir/{id}")
+    public String excluir(@PathVariable Integer id) {
+        clienteService.deleteById(id);
+        return "redirect:/clientes/listar";
+    }
+
+    // Método para exibir o formulário de edição do cliente
+    @GetMapping("/editar/{id}")
+    public String editar(@PathVariable Integer id, Model model) {
+        Cliente cliente = clienteService.findById(id);
+        model.addAttribute("cliente", cliente);
         return "cliente/formularioCliente";
     }
 }
--- src/main/java/br/com/petweb/petweb/service/ClienteService.java
+++ src/main/java/br/com/petweb/petweb/service/ClienteService.java
@@ -2,6 +2,7 @@ package br.com.petweb.petweb.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.petweb.petweb.entity.Cliente;
@@ -10,6 +11,7 @@ import br.com.petweb.petweb.repository.ClienteRepository;
 @Service
 public class ClienteService {
 
+    @Autowired
     private ClienteRepository clienteRepository;
 
     public ClienteService(ClienteRepository clienteRepository){
@@ -22,5 +24,15 @@ public class ClienteService {
 
     public List<Cliente> findAll(){
         return clienteRepository.findAll();
+    }
+
+    // Método para excluir um cliente por ID
+    public void deleteById(Integer id) {
+        clienteRepository.deleteById(id);
+    }
+
+    // Método para encontrar um cliente por ID
+    public Cliente findById(Integer id) {
+        return clienteRepository.findById(id).orElse(null);
     }
 }
